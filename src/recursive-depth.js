04bd51f9c3e41a805f130afca30408120a7afe9e
mDjeee <isejtniyazov@mail.ru>
2022-04-29 15:38:41 +0500

    feat: solve recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -14,17 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class DepthCalculator {
   calculateDepth(arr) {
-    throw new NotImplementedError('Not implemented');
-    if(Array.isArray(arr)){
-      arr = arr.map(item => this.calculateDepth(item));
-    }
-    return Array.isArray(arr) ? 1 + Math.max(...arr) : 0;
+    const that = this;
+    // throw new NotImplementedError('Not implemented');
+    return 1 + (Array.isArray(arr) ? arr.reduce((max,item) => Math.max(max, that.calculateDepth(item)), 0) : -1);
   }
 }
+
+
+
 module.exports = {
   DepthCalculator
 };
 
+
+// return 1 + (Array.isArray(arr) ? arr.reduce(function(max, item) {
+//   return Math.max(max, that.calculateDepth(item));
+// }, 0) : -1);
+
+
+// if (Array.isArray(arr)) return 1 + Math.max(...arr.map(t => that.calculateDepth(t)))
+// else return 0
+
+// return arr.reduce((count,item) => Array.isArray(item) ? 1 + this.calculateDepth(item) : count, 1)
+
+// return Array.isArray(arr) ? 1 + Math.max(...arr.map(item => this.calculateDepth(item))) : 0;
+
+// calculateDepth = (arr) => arr.reduce((count,v) => !Array.isArray(v) ? count : 1 + this.calculateDepth(v),1)
 
 // function doSth(arr){
 //   return (condition) ? 1 + Math.max(...arr.map(doSth)) : 0;
